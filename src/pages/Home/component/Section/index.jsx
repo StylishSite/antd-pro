2df298ef4fd9dc03c6ea2cc9362ab3d3db838cf7
zhangtianhua <zhangtianhua@zhaojiafang.com>
2020-04-30 11:53:05 +0800

    '提交'

--- src/pages/Home/component/Section/index.jsx
+++ src/pages/Home/component/Section/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-
+import { Row, Col } from 'antd';
+import MyAccount from './MyAccount';
+import SaleTrend from './SalesTrend';
+import MessageNotice from './MessageNotice';
 
 export default ({}) => {
   return (
-    'this is bootom display'
+    <Row gutter={24}>
+      <Col xs={{ span: 24 }} sm={{ span: 12 }} lg={{ span: 8 }}>
+        <MyAccount />
+      </Col>
+      <Col xs={{ span: 24 }} sm={{ span: 12 }} lg={{ span: 8 }}>
+        <SaleTrend />
+      </Col>
+      <Col xs={{ span: 24 }} sm={{ span: 12 }} lg={{ span: 8 }}>
+        <MessageNotice />
+      </Col>
+    </Row>
   )
 }
